8daca4a017d36c11aa8b30e680e070f49b392077
rishu1411 <mauryarishab87@gmail.com>
2023-11-10 23:09:10 +0530

    all issues resolved

--- src/app/ProductDetails/ProductDetails.jsx
+++ src/app/ProductDetails/ProductDetails.jsx
@@ -24,23 +24,24 @@ function ProductDetails() {
             {isLoading ?
                 (<div className='flex items-center justify-center h-screen'><RotateRightIcon className='animate-spin text-pink-400' /></div>) :
             
-                (<div className='container mx-auto xs:my-14 sm:my-0'>
-                    <div className='h-screen md:h-screen flex items-center justify-center sm:justify-start '>
+                (<div className='container mx-auto mt-10'>
+                    <div className='h-screen  flex flex-col xs:flex-row items-center justify-center sm:justify-start gap-x-8 gap-y-8 '>
                         {
                             View.map((item) => {
                                 return (
-                                    <div key={item.id} className='flex flex-col sm:flex-row items-center gap-x-8 gap-y-6'>
-                                        <div className='border py-10 px-5 shadow-md '><img src={item.image} alt="product image" className='h-44 lg:h-64' /></div>
+                                    <>
+                                        <img src={item.image} alt="product image" className='h-44 lg:h-64 mt-10' />
                                         <div className=' space-y-3'>
                                             <h3 className='sm:w-80 text-base sm:text-2xl  font-semibold'>{item.title}</h3>
                                             <p className='text-pink-500'>$ { item.price}</p>
-                                            <p className='sm:w-80 text-[12px] sm:text-sm overflow-y-auto max-h-60'><span className='font-semibold'>Description: </span>{ item.description}</p>
+                                            <p className='sm:w-80 text-[12px] sm:text-sm overflow-y-auto max-h-40 xs:max-h-60'><span className='font-semibold'>Description: </span>{ item.description}</p>
                                     
                                             <Button1 onClick={()=>addToCart(ID,items)} children='Add to Cart' className='px-2 w-auto py-1 rounded-sm text-white bg-black text-sm hover:bg-white hover:text-black border-2 border-black font-semibold transition duration-200'/>
                                         
                                         </div>
+                                        </>
 
-                                    </div>
+                                  
                                 )
                             })
                         }
